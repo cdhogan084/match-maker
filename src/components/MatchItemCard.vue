5c7ef50f21b292b1a4e6d768bf7b335a887eed32
Connor Hogan <ConnorHogan@quickenloans.com>
2019-10-03 11:50:32 -0400

    adding to github

---
+++ src/components/MatchItemCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card class="mx-auto">
+        <v-row v-for="n in 2" :key="n" :class="n === 1 ? 'mb-6' : ''" no-gutters>
+            <v-col v-for="k in n + 1" :key="k">
+                <v-card class="pa-2" outlined tile>{{ k }} of {{ n + 1 }}</v-card>
+            </v-col>
+        </v-row>
+        <v-card-actions>
+            <v-btn text>Edit</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "MatchItemCard",
+    data() {
+        return;
+    }
+};
+</script>
---
+++ src/components/MatchItemsContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container class="grey lighten-5">
+        <MatchItemCard />
+    </v-container>
+</template>
+<script>
+import MatchItemCard from "./MatchItemCard";
+
+export default {
+    name: "MatchItemsContainer",
+    components: {
+        MatchItemCard
+    },
+    data() {
+        return;
+    }
+};
+</script>
